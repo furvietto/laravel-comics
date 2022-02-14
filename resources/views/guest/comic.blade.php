aa444917be93d15391aa1f584eaff3dcadc33199
furvietto <fulvio-94@hotmail.it>
2022-02-14 20:13:21 +0100

    work done

--- resources/views/guest/comic.blade.php
+++ resources/views/guest/comic.blade.php
@@ -6,40 +6,137 @@
 
 @section('content')
     <main>
-        <div class="header-comic">
-           <img src="{{$comic["thumb"]}}" alt=""> 
-        </div>
-        <div class="main-comic">
-            <div class="main-comic-left">
-                <h2>{{$comic["title"]}}</h2>
-                <div class="price-container">
-                    <div class="price">
-                        <div>
-                            U.S Price: <span id="dollar-price">{{$comic["price"]}}</span>
+        <section>
+            <div class="header-comic">
+               <img src="{{$comic["thumb"]}}" alt=""> 
+            </div>
+        </section>
+
+        <section>
+            <div class="main-comic">
+                <div class="main-comic-left">
+                    <h2>{{$comic["title"]}}</h2>
+                    <div class="price-container">
+                        <div class="price">
+                            <div>
+                                U.S Price: <span id="dollar-price">{{$comic["price"]}}</span>
+                            </div>
+                            <div>
+                                AVAILABLE
+                            </div>
                         </div>
-                        <div>
-                            AVAILABLE
+                        <button>
+                            Check availability <i class="fas fa-sort-down"></i>
+                        </button>
+                    </div>
+                    <div class="description">
+                        {{$comic["description"]}}
+                    </div>
+                </div>
+                <div class="main-comic-rigth">
+                    <div class="advertisment">ADVERTISMENT</div>
+                    <img src="/img/adv.jpg" alt="">
+                </div>
+            </div>
+        </section>
+
+        <section class="bibliography-container">
+            <div class="bibliography">
+                <div class="talent-container">
+                    <div>
+                        <h3>Talent</h3>
+                    </div>
+                    <div class="cont-flex-bibliography">
+                        <div class="cont-flex-bibliography-l">
+                            Art by:
+                        </div>
+                        <div class="cont-flex-bibliography-r">
+                            @foreach ($comic["artists"] as $artist)
+                            @if ($loop->last)
+                            <a href="">{{$artist}}</a> 
+                            @else
+                            <a href="">{{$artist}},</a> 
+                            @endif
+                            @endforeach
                         </div>
                     </div>
-                    <button>
-                        Check availability <i class="fas fa-sort-down"></i>
-                    </button>
-                </div>
-                <div class="description">
-                    {{$comic["description"]}}
-                </div>
-            </div>
-            <div class="main-comic-rigth">
-                <img src="{{asset('img/adv.jpg')}}" alt="">
-            </div>
-            <div class="bibliography">
-                <div class="talent">
-
+                    <div class="cont-flex-bibliography">
+                        <div class="cont-flex-bibliography-l">
+                            Written by:
+                        </div>
+                        <div class="cont-flex-bibliography-r">
+                            @foreach ($comic["writers"] as $writer)
+                                    @if ($loop->last)
+                                    <a href="">{{$writer}}</a> 
+                                    @else
+                                    <a href="">{{$writer}},</a> 
+                                    @endif
+                                @endforeach
+                        </div>
+                    </div>
                 </div>
                 <div class="specs">
-
+                    <div>
+                        <h3>Specs</h3>
+                    </div>
+                    <div class="cont-flex-bibliography">
+                        <div class="cont-flex-bibliography-l">
+                            Series:
+                        </div>
+                        <div class="cont-flex-bibliography-r">
+                            <a href="">{{$comic["series"]}}</a>
+                        </div>
+                    </div>
+                    <div class="cont-flex-bibliography">
+                        <div class="cont-flex-bibliography-l">
+                            U.S. Price:
+                        </div>
+                        <div class="cont-flex-bibliography-r">
+                            {{$comic["price"]}}
+                        </div>
+                    </div>
+                    <div class="cont-flex-bibliography">
+                        <div class="cont-flex-bibliography-l">
+                            On Sale Date:
+                        </div>
+                        <div class="cont-flex-bibliography-r">
+                            {{$comic["sale_date"]}}
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
+        <section class="footer-comic">
+            <div class="footer-container">
+                <div>
+                    <div class="left-footer">
+                         DIGITAL COMICS   
+                    </div>
+                    <div class="pad-links">
+                    </div>
+                </div>
+                <div>
+                    <div class="left-footer">
+                        SHOP DC  
+                   </div>
+                   <div class="shop-links">
+                   </div>
+                </div>
+                <div>
+                    <div class="left-footer">
+                        COMIC SHOP LOCATOR 
+                   </div>
+                   <div class="locator-links">
+                   </div>
+                </div>
+                <div class="last-border">
+                    <div class="left-footer">
+                        SUBSCRIPTIONS
+                   </div>
+                   <div class="subscription-links">
+                   </div>
+                </div>
+            </div>
+        </section>
     </main>
 @endsection
